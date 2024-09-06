e44b7c95ce760dc80afefe45819294f4a63197b9
DP3PFome <166010240+DP3PFome@users.noreply.github.com>
2024-09-06 11:21:06 +0000

    uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,10 +1,26 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ievadit savu vardu");
+        String vards = scanner.nextLine();
+        
+
+
+
+        System.out.println("ievadit klase");
+        String klase = scanner.nextLine();
+
+        System.out.println("ievadit vecumu");
+        String vecums = scanner.nextLine();
+
+        
+System.out.println("Sveiki "+vards+" no "+klase+" klase"+" vecuma "+vecums+"!");
 
     }
 
