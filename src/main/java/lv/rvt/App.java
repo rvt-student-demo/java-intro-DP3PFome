0eb3123117f538742088192832c1e1b3348c9e6f
DP3PFome <166010240+DP3PFome@users.noreply.github.com>
2024-09-20 05:49:25 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,25 +5,37 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
+
     {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Give speed: ");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if (speed>120) {
+                System.out.println("Speeding ticket!");
+                
+        }
+
+
         //Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the first number: ");
-        int sk1 = Integer.valueOf(scanner.nextLine());
-        
-        System.out.println("Give the second number: ");
-        int sk2 = Integer.valueOf(scanner.nextLine());
-
-        int result = sk1 + sk2;
-        System.out.println( sk1+"+"+sk2+"="+result);
-
-        int atnem = sk1 - sk2;
-        System.out.println( sk1+"-"+sk2+"="+atnem);
-
-        int reiz = sk1 * sk2;
-        System.out.println( sk1+"*"+sk2+"="+reiz);
-
-        double dal = (double) sk1 / sk2;
-        System.out.println( sk1+"/"+sk2+"="+dal);
+     //   System.out.println("Give the first number: ");
+     //   int sk1 = Integer.valueOf(scanner.nextLine());
+     //   
+     //   System.out.println("Give the second number: ");
+     //   int sk2 = Integer.valueOf(scanner.nextLine());
+//
+     //   int result = sk1 + sk2;
+     //   System.out.println( sk1+"+"+sk2+"="+result);
+//
+     //   int atnem = sk1 - sk2;
+     //   System.out.println( sk1+"-"+sk2+"="+atnem);
+//
+     //   int reiz = sk1 * sk2;
+     //   System.out.println( sk1+"*"+sk2+"="+reiz);
+//
+     //   double dal = (double) sk1 / sk2;
+     //   System.out.println( sk1+"/"+sk2+"="+dal);
 
 
 
