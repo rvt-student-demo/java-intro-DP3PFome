bb20b1ff9dbd7ce5317cbede01607789fed73ea0
DP3PFome <166010240+DP3PFome@users.noreply.github.com>
2024-10-25 06:30:14 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,29 +9,51 @@ public class App
     {
         
         Scanner scanner = new Scanner(System.in);
-        int sum = 0 ;
+        
         int x=2;
-        ArrayList<Integer> wordList= new ArrayList();
+        ArrayList<Integer> numbers= new ArrayList();
 
+
+        System.out.println("Input numbers: ");
         while (x!=-1) {
             Integer i = Integer.valueOf(scanner.nextLine());
             x=i;
-            wordList.add(i);
+            numbers.add(i);
             
             
         }
 
+        Integer start;
+        Integer end;
+
+        while (true) {
+            System.out.print("From where? ");
+            start = Integer.valueOf(scanner.nextLine());
         
-        System.out.print("From where? ");
-        Integer fr = Integer.valueOf(scanner.nextLine());
+            
+            System.out.print("To where? ");
+            end = Integer.valueOf(scanner.nextLine());
     
-        
-        System.out.print("To where? ");
-        Integer  to = Integer.valueOf(scanner.nextLine());
+            if (start < 0 || start > numbers.size() - 1) {
+                System.out.println("Invalid start index");
+                
+            } 
+            else if (start > end) {
+                System.out.println("Start index can not be greater then end index");
+               
+            }
+            else if (end <= 0 || end > numbers.size() - 1) {
+                System.out.println("Invalid end index");
+            }
+            else
+            { break; }
+            
+        }
         
 
-        for (int i = fr;i <to;i++ ){
-            System.out.println(wordList.get(i));
+        // Tas mums izpildas kad visi indeksi ir pareizi
+        for (int i = start;i < end ;i++ ){
+            System.out.println(numbers.get(i));
 
         }
 
