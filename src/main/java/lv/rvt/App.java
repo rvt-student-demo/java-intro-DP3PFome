7afc8e636033247d2ad26f229a7ad7f0314d089e
DP3PFome <166010240+DP3PFome@users.noreply.github.com>
2024-10-25 06:08:57 +0000

    Only these numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,13 +1,41 @@
+
 package lv.rvt;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class App 
+
 {
     public static void main( String[] args )
     {
-        //Stars.printSquare(4);
-        //Stars.printRectangle(17, 3);
-        Stars.christmasTree(10);
+        
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0 ;
+        int x=2;
+        ArrayList<Integer> wordList= new ArrayList();
 
+        while (x!=-1) {
+            Integer i = Integer.valueOf(scanner.nextLine());
+            x=i;
+            wordList.add(i);
+            
+            
+        }
+
+        
+        System.out.print("From where? ");
+        Integer fr = Integer.valueOf(scanner.nextLine());
+    
+        
+        System.out.print("To where? ");
+        Integer  to = Integer.valueOf(scanner.nextLine());
+        
+
+        for (int i = fr;i <to;i++ ){
+            System.out.println(wordList.get(i));
+
+        }
+
+        
 
 
     }
