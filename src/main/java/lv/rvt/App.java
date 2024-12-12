b489cfdf12a70767e8724bea8791f558ddf0557f
DP3PFome <166010240+DP3PFome@users.noreply.github.com>
2024-12-12 11:23:29 +0000

    Card payments (1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,33 +4,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    
-   
-    
     public static void main(String[] args) {
+        PaymentCard petesCard = new PaymentCard(10.0);
 
-        HealthStation childrensHospital = new HealthStation();
-    
-        Person ethan = new Person("Ethan", 1, 110, 7);
-        Person peter = new Person("Peter", 33, 176, 85);
-    
-        System.out.println("weighings performed: " + childrensHospital.weighings());
-    
-        childrensHospital.weigh(ethan);
-        childrensHospital.weigh(peter);
-    
-        System.out.println("weighings performed: " + childrensHospital.weighings());
-    
-        childrensHospital.weigh(ethan);
-        childrensHospital.weigh(ethan);
-        childrensHospital.weigh(ethan);
-        childrensHospital.weigh(ethan);
-    
-        System.out.println("weighings performed: " + childrensHospital.weighings());
+        System.out.println("money " + petesCard.balance());
+        Boolean wasSuccessful = petesCard.takeMoney(8.0);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
+
+        wasSuccessful = petesCard.takeMoney(4);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
     }
-    
-
-
 }
 
 
---
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -0,0 +1,28 @@
+package lv.rvt;
+public class PaymentTerminal {
+    private double money;  // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals;  // number of sold hearty meals
+
+    public PaymentTerminal() {
+        this.ma
+        // register initially has 1000 euros of money
+    }
+
+    public double eatAffordably(double payment) {
+        // an affordable meal costs 2.50 euros
+        // increase the amount of cash by the price of an affordable meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+    }
+
+    public double eatHeartily(double payment) {
+        // a hearty meal costs 4.30 euros
+        // increase the amount of cash by the price of a hearty meal and return the change
+        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
+
